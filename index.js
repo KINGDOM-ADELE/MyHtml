78cd48ff1b238fcd54f3e8d5ece9e6a82957be34
KINGDOM-ADELE <izzylovu@gmail.com>
2024-08-25 10:37:56 +0100

    testing git lens

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+
+const unusedVariable = "I am not used;
+
+console.log("This is a message";
+
+function greet( {
+  console.log("Hello, " + name);
+}
+
+let greeting = "Hello, "  name + "!";
+
+setTimeout(function () {
+  console.log("This is a delayed message");
+} 1000);
